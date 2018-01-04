d7b02cc72ab19a2c084f9c4c928d13a099448221
KiwiLss <m15205622327@163.com>
2018-01-04 09:54:41 +0800

    1

--- app/src/main/java/com/magicsoft/testcode/MainActivity.java
+++ app/src/main/java/com/magicsoft/testcode/MainActivity.java
@@ -1,7 +1,7 @@
 package com.magicsoft.testcode;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
