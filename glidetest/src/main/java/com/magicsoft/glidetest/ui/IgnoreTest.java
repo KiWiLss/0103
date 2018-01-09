0bd583c89001c10d375807819885cf77581109f6
KiwiLss <m15205622327@163.com>
2018-01-09 17:49:29 +0800

    9

---
+++ glidetest/src/main/java/com/magicsoft/glidetest/ui/IgnoreTest.java
@@ -0,0 +1,15 @@
+package com.magicsoft.glidetest.ui;
+
+/**
+ * <pre>
+ *     author : Lss winding
+ *     e-mail : kiwilss@163.com
+ *     time   : 2018/1/9
+ *     desc   : ${DESCRIPTION}
+ *     version: ${VERSION}
+ * </pre>
+ */
+
+
+public class IgnoreTest {
+}
